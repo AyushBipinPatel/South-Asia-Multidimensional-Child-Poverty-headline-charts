4e1e89412e9e9ee1769576247d3ffdb895bee253
AyushBipinPatel <ayush.ap58@gmail.com>
2021-06-24 16:17:37 +0530

    chart 3 result B - code and output along with combined col charts for result B

--- scripts/generate charts results B.R
+++ scripts/generate charts results B.R
@@ -121,3 +121,61 @@ ggsave(
 
 ### Malnourished and MPI poor
 
+
+data_result_b %>% 
+  mutate(
+    Country = fct_reorder(
+      as.factor(Country),
+      perc_ch_04_maln_mpi_poor
+    )
+  ) %>% 
+  ggplot(aes(Country,
+             perc_ch_04_maln_mpi_poor))+
+  geom_col(aes(fill = num_ch_04_maln))+
+  geom_text(aes(label = paste(
+    round(num_ch_04_maln/1000,2),
+    "(K)"
+  ),
+  colour = num_ch_04_maln),
+  nudge_y = 1)+
+  geom_text(aes(label = paste(
+    perc_ch_04_maln_mpi_poor,"(%)"
+  )),
+  nudge_y = 2.5)+
+  scale_fill_gradient(low = "#8eb584" ,
+                      high = "#ff010d")+
+  scale_color_gradient(low = "#8eb584",
+                       high = "#ff010d",
+                       label = comma)+
+  scale_y_continuous(labels = percent_format(scale = 1))+
+  scale_x_discrete(limits = c("Maldives","Bhutan","Nepal",
+                              "Pakistan","India","Bangladesh"))+
+  guides(fill = FALSE,
+         colour = guide_colorbar(
+           title = "Number of children (aged 0-4) who are MPI poor and manourised",
+           title.position = "top",
+           barwidth = 20
+           #label.position = "bottom"
+         ))+
+  labs(
+    title = "MPI poor and malnourised children (aged 0-4) in South Asia",
+    subtitle = "Number of MPI poor and malnourised children in <span style = 'color:#ff010d;'>India</span> __is equal to 2.44 times__ the sum of malnourised children <br>in <span style = 'color:#8eb584;'>Maldives</span>, <span style = 'color:#8eb584;'>Nepal</span>, <span style = 'color:#8eb584;'>Bhutan</span>, <span style = 'color:#ae9613;'>Pakistan</span> and <span style = 'color:#9ca442'>Bangladesh</span>",
+    y = "MPI poor and Malnourised Children (%)",
+    x = NULL
+  )+
+  theme(
+    panel.background = element_rect(fill = "#ffffff"),
+    panel.grid = element_line(colour = "grey"),
+    legend.position = c(0.4,.94),
+    legend.direction = "horizontal",
+    legend.background = element_rect(fill = NA),
+    plot.title.position = "plot",
+    plot.subtitle = element_markdown(size = 12),
+    
+  )+
+  coord_cartesian(ylim = c(0,40))-> chart_ch_malnourish
+
+
+ggsave(
+  here("charts/result_b_chart_child_mpi_poor_and_malnourished.png"),
+  device = "png",width = 14,height = 8,units = "in")
